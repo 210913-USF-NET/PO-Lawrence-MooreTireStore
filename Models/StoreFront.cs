8a331c67ddeab2271a29b62dda8dbaa4097b7e7c
A Lot Moore Inc <moore_lawrence@columbusstate.edu>
2021-09-16 22:13:13 -0400

    Revisions

---
+++ Models/StoreFront.cs
@@ -0,0 +1,18 @@
+namespace Models
+{
+    public class StoreFront
+    {
+        
+        public StoreFront() {}
+
+        public string Name { get; set;}
+
+        public string Address { get; set;}
+
+        public override string ToString(){
+            return $"Store Name: {this.Name} \nAddress: {this.Address}"; 
+
+            
+        }
+    }
+}
